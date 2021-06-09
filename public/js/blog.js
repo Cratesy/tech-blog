77ae3d272d91f10e95d4b486ce2529f7f6863b68
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-06-09 18:53:02 +0100

    added blogs

---
+++ public/js/blog.js
@@ -0,0 +1,50 @@
+const onDelete = async (event) => {
+  const id = event.currentTarget.id;
+
+  const options = {
+    method: "DELETE",
+    redirect: "follow",
+  };
+
+  const response = await fetch(`/api/blogs/${id}`, options);
+
+  if (response.status !== 200) {
+    console.log("FAILED DELETE");
+  } else {
+    window.location.replace("/dashboard");
+  }
+};
+
+const onEditBlog = async (event) => {
+  event.preventDefault();
+
+  const id = event.currentTarget.id;
+
+  const title = $("#title").val();
+  const description = $("#description").val();
+  const status = $("#status").find(":selected").val();
+
+  const options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    body: JSON.stringify({
+      title,
+      description,
+      status,
+    }),
+  };
+
+  const response = await fetch(`/api/blogs/${id}`, options);
+
+  if (response.status !== 200) {
+    console.log("FAILED UPDATE");
+  } else {
+    window.location.replace("/dashboard");
+  }
+};
+
+$('[name="delete-btn"]').click(onDelete);
+$('[name="edit-blog-form"]').submit(onEditBlog);
