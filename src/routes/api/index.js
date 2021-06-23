bb6d1250033ca0c5de017879610daaa5a06d078c
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-06-23 16:36:21 +0100

    delete comment

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 
 const blogsRoutes = require("./blogs");
+const commentRoutes = require("./comments")
 
 const router = Router();
 
 router.use("/blogs", blogsRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
